bb4a58dff70c92244853bd3960fdf064abcbafb4
Ryan Liam <ryanliampy@gmail.com>
2023-05-09 23:27:08 +0800

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,163 +1,163 @@
-import emoji from 'react-easy-emoji';
+import emoji from "react-easy-emoji";
 
 export const greetings = {
-  name: 'Ryan Liam',
+  name: "Ryan Liam",
   title: "Hi, I'm Ryan Liam",
   description:
-    'I am a Year 3 student studying for a Diploma in Data Science at Ngee Ann Polytechnic. Through my course of study, I am well equipped with a variety of Data Visualization tools, Data Cleaning, Data Transformation and Machine Learning techniques. Currently, I am looking for internship opportunities to enhance my Data Science skills and gain real-life work experience.',
+    "I am a Year 3 student studying for a Diploma in Data Science at Ngee Ann Polytechnic. Through my course of study, I am well equipped with a variety of Data Visualization tools, Data Cleaning, Data Transformation and Machine Learning techniques. Currently, I am looking for internship opportunities to enhance my Data Science skills and gain real-life work experience.",
   resumeLink:
-    'https://docs.google.com/document/d/1_4RIfbyt4FFHW_bNplnoINQr01neTu7XDeeUlT_2O_k/edit?usp=sharing',
+    "https://docs.google.com/document/d/1_4RIfbyt4FFHW_bNplnoINQr01neTu7XDeeUlT_2O_k/edit?usp=sharing",
 };
 
 export const openSource = {
-  githubUserName: 'R-otato',
+  githubUserName: "R-otato",
 };
 
 export const contact = {};
 
 export const socialLinks = {
-  linkedin: 'https://www.linkedin.com/in/ryan-liam-1a37bb203',
-  github: 'https://github.com/R-otato',
-  instagram: 'https://www.instagram.com/r._.liam/',
+  linkedin: "https://www.linkedin.com/in/ryan-liam-1a37bb203",
+  github: "https://github.com/R-otato",
+  instagram: "https://www.instagram.com/r._.liam/",
 };
 
 export const skillsSection = {
-  title: 'What I do',
+  title: "What I do",
   subTitle:
-    'Passionate Data Science Student Exploring New Techniques And Technologies',
+    "Passionate Data Science Student Exploring New Techniques And Technologies",
   data: [
     {
-      title: 'Data Exploration and Wrangling',
-      lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
+      title: "Data Exploration and Wrangling",
+      lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
       skills: [
         emoji(
-          '⚡ Creating Interactive Data Visualizations with Jupyter Notebook, Power BI, Tableau and SAP Analytics Cloud'
+          "⚡ Creating Interactive Data Visualizations with Jupyter Notebook, Power BI, Tableau and SAP Analytics Cloud"
         ),
         emoji(
-          '⚡ Creating vizualizations in Jupyter Notebook using libraries such as Matplotlib, Seaborn and Plotly'
+          "⚡ Creating vizualizations in Jupyter Notebook using libraries such as Matplotlib, Seaborn and Plotly"
         ),
-        emoji('⚡ Automating Data Preparation and Workflow with Alteryx.'),
+        emoji("⚡ Automating Data Preparation and Workflow with Alteryx."),
         emoji(
-          '⚡ Writing complex SQL queries for data exploration and analysis'
+          "⚡ Writing complex SQL queries for data exploration and analysis"
         ),
       ],
       softwareSkills: [
         {
-          skillName: 'Jupyter Notebook',
-          fontAwesomeClassname: 'logos:jupyter',
+          skillName: "Jupyter Notebook",
+          fontAwesomeClassname: "logos:jupyter",
         },
 
         {
-          skillName: 'Matplotlib',
-          fontAwesomeClassname: 'logos:matplotlib',
+          skillName: "Matplotlib",
+          fontAwesomeClassname: "logos:matplotlib",
         },
 
         {
-          skillName: 'Seaborn',
-          fontAwesomeClassname: 'logos:seaborn',
+          skillName: "Seaborn",
+          fontAwesomeClassname: "logos:seaborn",
         },
 
         {
-          skillName: 'Plotly',
-          fontAwesomeClassname: 'devicon:plotly-wordmark',
+          skillName: "Plotly",
+          fontAwesomeClassname: "devicon:plotly-wordmark",
         },
 
         {
-          skillName: 'Power BI',
-          fontAwesomeClassname: 'logos:microsoft-power-bi',
+          skillName: "Power BI",
+          fontAwesomeClassname: "logos:microsoft-power-bi",
         },
 
         {
-          skillName: 'Tableau',
-          fontAwesomeClassname: 'logos:tableau-icon',
+          skillName: "Tableau",
+          fontAwesomeClassname: "logos:tableau-icon",
         },
 
         {
-          skillName: 'SAP Analytics Cloud',
-          fontAwesomeClassname: 'logos:sap',
+          skillName: "SAP Analytics Cloud",
+          fontAwesomeClassname: "logos:sap",
         },
 
         {
-          skillName: 'Alteryx',
-          fontAwesomeClassname: 'mdi:alpha-a-box-outline',
+          skillName: "Alteryx",
+          fontAwesomeClassname: "mdi:alpha-a-box-outline",
         },
 
         {
-          skillName: 'SQL',
-          fontAwesomeClassname: 'carbon:sql',
+          skillName: "SQL",
+          fontAwesomeClassname: "carbon:sql",
         },
       ],
     },
     {
-      title: 'Machine-Learning',
+      title: "Machine-Learning",
       lottieAnimationFile:
-        '/lottie/skills/143472-mapping-for-machine-learning.json', // Path of Lottie Animation JSON File
+        "/lottie/skills/143472-mapping-for-machine-learning.json", // Path of Lottie Animation JSON File
       skills: [
         emoji(
-          '⚡ Building supervised learning models in scikit-learn and PySpark'
+          "⚡ Building supervised learning models in scikit-learn and PySpark"
         ),
-        emoji('⚡ Building unsupervised learning models in scikit-learn'),
-        emoji('⚡ Performing Feature selection and engineering techniques'),
+        emoji("⚡ Building unsupervised learning models in scikit-learn"),
+        emoji("⚡ Performing Feature selection and engineering techniques"),
 
-        emoji('⚡ Performing Model tuning and hyperparameter optimization'),
+        emoji("⚡ Performing Model tuning and hyperparameter optimization"),
 
         emoji(
-          '⚡ Understanding Evaluation metrics for machine learning models'
+          "⚡ Understanding Evaluation metrics for machine learning models"
         ),
       ],
       softwareSkills: [
         {
-          skillName: 'scikit-learn',
-          fontAwesomeClassname: 'simple-icons:scikit-learn',
+          skillName: "scikit-learn",
+          fontAwesomeClassname: "simple-icons:scikit-learn",
         },
         {
-          skillName: 'PySpark',
-          fontAwesomeClassname: 'cib:apache-spark',
+          skillName: "PySpark",
+          fontAwesomeClassname: "cib:apache-spark",
         },
       ],
     },
     {
-      title: 'Cloud Infra-Architecture',
-      lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
+      title: "Cloud Infra-Architecture",
+      lottieAnimationFile: "/lottie/skills/cloudinfra.json", // Path of Lottie Animation JSON File
       skills: [
-        emoji('⚡ Experience of working on multiple cloud platforms'),
+        emoji("⚡ Experience of working on multiple cloud platforms"),
 
         emoji(
-          '⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases'
+          "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases"
         ),
       ],
       softwareSkills: [
         {
-          skillName: 'AWS',
-          fontAwesomeClassname: 'logos:aws',
+          skillName: "AWS",
+          fontAwesomeClassname: "logos:aws",
         },
         {
-          skillName: 'Azure',
-          fontAwesomeClassname: 'logos:microsoft-azure',
+          skillName: "Azure",
+          fontAwesomeClassname: "logos:microsoft-azure",
         },
         {
-          skillName: 'Snowflake',
-          fontAwesomeClassname: 'tabler:brand-snowflake',
+          skillName: "Snowflake",
+          fontAwesomeClassname: "tabler:brand-snowflake",
         },
         {
-          skillName: 'PostgreSQL',
-          fontAwesomeClassname: 'logos:postgresql',
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "logos:postgresql",
         },
         {
-          skillName: 'Github',
-          fontAwesomeClassname: 'akar-icons:github-fill',
+          skillName: "Github",
+          fontAwesomeClassname: "akar-icons:github-fill",
         },
         {
-          skillName: 'Docker',
-          fontAwesomeClassname: 'logos:docker-icon',
+          skillName: "Docker",
+          fontAwesomeClassname: "logos:docker-icon",
         },
         {
-          skillName: 'GCP',
-          fontAwesomeClassname: 'skill-icons:gcp-light',
+          skillName: "GCP",
+          fontAwesomeClassname: "skill-icons:gcp-light",
         },
         {
-          skillName: 'Airbyte',
-          fontAwesomeClassname: 'simple-icons:airbyte',
+          skillName: "Airbyte",
+          fontAwesomeClassname: "simple-icons:airbyte",
         },
       ],
     },
@@ -166,39 +166,39 @@ export const skillsSection = {
 
 export const SkillBars = [
   {
-    Stack: 'Machine Learning',
-    progressPercentage: '90',
+    Stack: "Machine Learning",
+    progressPercentage: "90",
   },
   {
-    Stack: 'Data Wrangling',
-    progressPercentage: '90',
+    Stack: "Data Wrangling",
+    progressPercentage: "90",
   },
   {
-    Stack: 'Data Visualization',
-    progressPercentage: '80',
+    Stack: "Data Visualization",
+    progressPercentage: "80",
   },
   {
-    Stack: 'Cloud Computing',
-    progressPercentage: '60',
+    Stack: "Cloud Computing",
+    progressPercentage: "60",
   },
   {
-    Stack: 'Deep Learning',
-    progressPercentage: '10',
+    Stack: "Deep Learning",
+    progressPercentage: "10",
   },
 ];
 
 export const educationInfo = [
   {
-    schoolName: 'Ngee Ann Polytechnic',
-    subHeader: 'Diploma in Data Science',
-    duration: 'April 2021 - Present',
-    desc: 'Participating in the Snowflake x NP Industry Currency Project.',
-    grade: 'GPA: 3.90',
+    schoolName: "Ngee Ann Polytechnic",
+    subHeader: "Diploma in Data Science",
+    duration: "April 2021 - Present",
+    desc: "Participating in the Snowflake x NP Industry Currency Project.",
+    grade: "GPA: 3.90",
     descBullets: [
-      'Offer a user-friendly customer analytics platform called Tasty Insights for the food truck business Tasty Bytes',
-      'Create a streamlit application that provides key insights on customer behavior to improve retention and drive growth for Tasty Bytes Marketing and Sales Team',
-      'Create an unsupervised learning model to perform customer segmentation',
-      'Create a supervised learning model to predict the number of days to next purchase for customers',
+      "Offer a user-friendly customer analytics platform called Tasty Insights for the food truck business Tasty Bytes",
+      "Create a streamlit application that provides key insights on customer behavior to improve retention and drive growth for Tasty Bytes Marketing and Sales Team",
+      "Create an unsupervised learning model to perform customer segmentation",
+      "Create a supervised learning model to predict the number of days to next purchase for customers",
     ],
   },
 ];
@@ -240,14 +240,24 @@ export const educationInfo = [
 
 export const projects = [
   {
-    name: 'Formula One Predictor',
-    desc: 'This project is aimed towards predicting the top half F1 GP winners based on information before the race',
-    github: 'https://github.com/R-otato/Formula-one-prediction',
+    name: "Formula One Predictor",
+    desc: "This project is aimed towards predicting the top half F1 GP winners based on information before the race.",
+    descBullets: [
+      "Data Vizualization",
+      "Data Cleaning",
+      "Categorical Encoding",
+      "Numerical Transformation",
+      "Feature Engineering",
+      "Feature Scaling",
+      "Machine Learning",
+      "Feature Importance",
+    ],
+    github: "https://github.com/R-otato/Formula-one-prediction",
   },
   {
-    name: 'Singapore HDB Resale Price Predictor',
-    desc: 'This project aims to create a machine-learning model for predicting the resale prices of HDB flats using their properties from the data given. Using the model, we can find how the selling price of an HDB resale flat changes based on the information given.',
-    github: 'https://github.com/R-otato/PySpark-HDB-resale-prediction',
+    name: "Singapore HDB Resale Price Predictor",
+    desc: "This project aims to create a machine-learning model for predicting the resale prices of HDB flats using their properties from the data given. Using the model, we can find how the selling price of an HDB resale flat changes based on the information given.",
+    github: "https://github.com/R-otato/PySpark-HDB-resale-prediction",
   },
 ];
 
@@ -266,6 +276,6 @@ export const projects = [
 
 // See object prototype on SEO.jsx page
 export const seoData = {
-  title: 'Ryan Liam',
-  description: 'A passionate Data Science Student.',
+  title: "Ryan Liam",
+  description: "A passionate Data Science Student.",
 };
