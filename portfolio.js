6e8c1cbe3eecb5d6e50bae8982359f3af9bfbddc
Ryan Liam <ryanliampy@gmail.com>
2023-05-09 23:20:34 +0800

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,53 +1,118 @@
 import emoji from 'react-easy-emoji';
 
 export const greetings = {
-  name: 'Leong Kai Joon',
-  title: "Hi all, I'm Kai Joon",
+  name: 'Ryan Liam',
+  title: "Hi, I'm Ryan Liam",
   description:
-    "I'm a passionate Full Stack web developer having some experience of web applications with Javascript, HTML, CSS, and some Blockchain development on Ethereum (Solidity) and mobile applications with React Native",
-  resumeLink: '',
+    'I am a Year 3 student studying for a Diploma in Data Science at Ngee Ann Polytechnic. Through my course of study, I am well equipped with a variety of Data Visualization tools, Data Cleaning, Data Transformation and Machine Learning techniques. Currently, I am looking for internship opportunities to enhance my Data Science skills and gain real-life work experience.',
+  resumeLink:
+    'https://docs.google.com/document/d/1_4RIfbyt4FFHW_bNplnoINQr01neTu7XDeeUlT_2O_k/edit?usp=sharing',
 };
 
 export const openSource = {
-  githubUserName: 'darwin098',
+  githubUserName: 'R-otato',
 };
 
 export const contact = {};
 
 export const socialLinks = {
-  github: 'https://github.com/darwin098',
-  linkedin: 'https://www.linkedin.com/in/kai-joon-leong-baa646148/',
+  linkedin: 'https://www.linkedin.com/in/ryan-liam-1a37bb203',
+  github: 'https://github.com/R-otato',
+  instagram: 'https://www.instagram.com/r._.liam/',
 };
 
 export const skillsSection = {
   title: 'What I do',
   subTitle:
-    'CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK AND UPCOMING TECHNOLOGIES',
+    'Passionate Data Science Student Exploring New Techniques And Technologies',
   data: [
     {
-      title: 'Full Stack Development',
+      title: 'Data Exploration and Wrangling',
       lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
       skills: [
         emoji(
-          '⚡ Building responsive Full stack web applications with vanilla JavaScript'
+          '⚡ Creating Interactive Data Visualizations with Jupyter Notebook, Power BI, Tableau and SAP Analytics Cloud'
+        ),
+        emoji(
+          '⚡ Creating vizualizations in Jupyter Notebook using libraries such as Matplotlib, Seaborn and Plotly'
+        ),
+        emoji('⚡ Automating Data Preparation and Workflow with Alteryx.'),
+        emoji(
+          '⚡ Writing complex SQL queries for data exploration and analysis'
         ),
       ],
       softwareSkills: [
         {
-          skillName: 'HTML-5',
-          fontAwesomeClassname: 'vscode-icons:file-type-html',
+          skillName: 'Jupyter Notebook',
+          fontAwesomeClassname: 'logos:jupyter',
+        },
+
+        {
+          skillName: 'Matplotlib',
+          fontAwesomeClassname: 'logos:matplotlib',
+        },
+
+        {
+          skillName: 'Seaborn',
+          fontAwesomeClassname: 'logos:seaborn',
+        },
+
+        {
+          skillName: 'Plotly',
+          fontAwesomeClassname: 'devicon:plotly-wordmark',
+        },
+
+        {
+          skillName: 'Power BI',
+          fontAwesomeClassname: 'logos:microsoft-power-bi',
+        },
+
+        {
+          skillName: 'Tableau',
+          fontAwesomeClassname: 'logos:tableau-icon',
+        },
+
+        {
+          skillName: 'SAP Analytics Cloud',
+          fontAwesomeClassname: 'logos:sap',
+        },
+
+        {
+          skillName: 'Alteryx',
+          fontAwesomeClassname: 'mdi:alpha-a-box-outline',
+        },
+
+        {
+          skillName: 'SQL',
+          fontAwesomeClassname: 'carbon:sql',
+        },
+      ],
+    },
+    {
+      title: 'Machine-Learning',
+      lottieAnimationFile:
+        '/lottie/skills/143472-mapping-for-machine-learning.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji(
+          '⚡ Building supervised learning models in scikit-learn and PySpark'
+        ),
+        emoji('⚡ Building unsupervised learning models in scikit-learn'),
+        emoji('⚡ Performing Feature selection and engineering techniques'),
+
+        emoji('⚡ Performing Model tuning and hyperparameter optimization'),
+
+        emoji(
+          '⚡ Understanding Evaluation metrics for machine learning models'
+        ),
+      ],
+      softwareSkills: [
+        {
+          skillName: 'scikit-learn',
+          fontAwesomeClassname: 'simple-icons:scikit-learn',
         },
         {
-          skillName: 'CSS-3',
-          fontAwesomeClassname: 'vscode-icons:file-type-css',
-        },
-        {
-          skillName: 'JavaScript',
-          fontAwesomeClassname: 'logos:javascript',
-        },
-        {
-          skillName: 'NPM',
-          fontAwesomeClassname: 'logos:npm-icon',
+          skillName: 'PySpark',
+          fontAwesomeClassname: 'cib:apache-spark',
         },
       ],
     },
@@ -55,10 +120,11 @@ export const skillsSection = {
       title: 'Cloud Infra-Architecture',
       lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
       skills: [
+        emoji('⚡ Experience of working on multiple cloud platforms'),
+
         emoji(
           '⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases'
         ),
-        emoji('⚡ Building servers with SQL (PostgreSQL, MySQL, MicrosoftSQL)'),
       ],
       softwareSkills: [
         {
@@ -66,39 +132,32 @@ export const skillsSection = {
           fontAwesomeClassname: 'logos:aws',
         },
         {
-          skillName: 'Heroku',
-          fontAwesomeClassname: 'logos:heroku-icon',
+          skillName: 'Azure',
+          fontAwesomeClassname: 'logos:microsoft-azure',
+        },
+        {
+          skillName: 'Snowflake',
+          fontAwesomeClassname: 'tabler:brand-snowflake',
         },
         {
           skillName: 'PostgreSQL',
           fontAwesomeClassname: 'logos:postgresql',
         },
-      ],
-    },
-    {
-      title: 'Blockchain',
-      lottieAnimationFile: '/lottie/skills/ethereum.json', // Path of Lottie Animation JSON File
-      skills: [
-        emoji(
-          '⚡ Experience in developing simple Smart Contract using Solidity & Ethereum'
-        ),
-      ],
-      softwareSkills: [
         {
-          skillName: 'Ethereum',
-          fontAwesomeClassname: 'logos:ethereum',
+          skillName: 'Github',
+          fontAwesomeClassname: 'akar-icons:github-fill',
         },
         {
-          skillName: 'Solidity',
-          fontAwesomeClassname: 'logos:solidity',
+          skillName: 'Docker',
+          fontAwesomeClassname: 'logos:docker-icon',
         },
         {
-          skillName: 'Metamask',
-          fontAwesomeClassname: 'logos:metamask-icon',
+          skillName: 'GCP',
+          fontAwesomeClassname: 'skill-icons:gcp-light',
         },
         {
-          skillName: 'Ganache',
-          fontAwesomeClassname: 'logos:ganache-icon',
+          skillName: 'Airbyte',
+          fontAwesomeClassname: 'simple-icons:airbyte',
         },
       ],
     },
@@ -107,81 +166,106 @@ export const skillsSection = {
 
 export const SkillBars = [
   {
-    Stack: 'Frontend/Design', //Insert stack or technology you have experience in
-    progressPercentage: '75', //Insert relative proficiency in percentage
+    Stack: 'Machine Learning',
+    progressPercentage: '90',
   },
   {
-    Stack: 'Backend',
+    Stack: 'Data Wrangling',
+    progressPercentage: '90',
+  },
+  {
+    Stack: 'Data Visualization',
     progressPercentage: '80',
   },
   {
-    Stack: 'Programming',
-    progressPercentage: '70',
+    Stack: 'Cloud Computing',
+    progressPercentage: '60',
+  },
+  {
+    Stack: 'Deep Learning',
+    progressPercentage: '10',
   },
 ];
 
 export const educationInfo = [
   {
-    schoolName: 'Singapore Polytechnic',
-    subHeader: 'Pursuing Diploma in Information Technology',
-    duration: 'June 2021 - Present Date',
-    desc: '',
-    descBullets: ['Expected to finish education by June 2024'],
+    schoolName: 'Ngee Ann Polytechnic',
+    subHeader: 'Diploma in Data Science',
+    duration: 'April 2021 - Present',
+    desc: 'Participating in the Snowflake x NP Industry Currency Project.',
+    grade: 'GPA: 3.90',
+    descBullets: [
+      'Offer a user-friendly customer analytics platform called Tasty Insights for the food truck business Tasty Bytes',
+      'Create a streamlit application that provides key insights on customer behavior to improve retention and drive growth for Tasty Bytes Marketing and Sales Team',
+      'Create an unsupervised learning model to perform customer segmentation',
+      'Create a supervised learning model to predict the number of days to next purchase for customers',
+    ],
   },
 ];
 
-export const experience = [
-  //   {
-  //     role: 'API Engineer',
-  //     company: 'Duseca Software',
-  //     companylogo: '/img/icons/common/dusecaSoftware.jpg',
-  //     date: 'Jan 2022 – Mar 2022',
-  //     desc: 'I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.',
-  //     // descBullets: [
-  //     // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-  //     // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-  //     // ],
-  //   },
-  //   {
-  //     role: 'Full Stack Developer',
-  //     company: 'Bleed-AI',
-  //     companylogo: '/img/icons/common/bleedAI.jpg',
-  //     date: 'Sept 2021 - Oct 2021',
-  //     desc: 'Worked as django web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.',
-  //   },
-  //   {
-  //     role: 'Backend Developer',
-  //     company: 'Wapidu',
-  //     companylogo: '/img/icons/common/wapidu.jpg',
-  //     date: 'Sept 2021',
-  //     desc: 'Worked as a Django Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.',
-  //   },
-];
+// export const experience = [
+//   {
+//     role: 'Frontend Developer',
+//     company: 'Duseca Software',
+//     companylogo: '/img/icons/common/dusecaSoftware.jpg',
+//     date: 'Apr 2022 – Jun 2022',
+//     desc: 'I worked as frontend web developer to design web based admin dashboards for mulitple apps using Django, Bootstrap, jQuery, Chart.js and some other libraries.',
+//   },
+//   {
+//     role: 'API Engineer',
+//     company: 'Duseca Software',
+//     companylogo: '/img/icons/common/dusecaSoftware.jpg',
+//     date: 'Jan 2022 – Mar 2022',
+//     desc: 'I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.',
+//     // descBullets: [
+//     // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+//     // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+//     // ],
+//   },
+//   {
+//     role: 'Full Stack Developer',
+//     company: 'Bleed-AI',
+//     companylogo: '/img/icons/common/bleedAI.jpg',
+//     date: 'Sept 2021 - Oct 2021',
+//     desc: 'Worked as django web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.',
+//   },
+//   {
+//     role: 'Backend Developer',
+//     company: 'Wapidu',
+//     companylogo: '/img/icons/common/wapidu.jpg',
+//     date: 'Sept 2021',
+//     desc: 'Worked as a Django Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.',
+//   },
+// ];
 
 export const projects = [
   {
-    name: 'Water Vapor (School Group Project)',
-    desc: 'With Water Vapor, you can easily find games that are on the steam store for a great price!',
-    link: 'https://ades-team-5.netlify.app/',
-    text: 'Demo',
+    name: 'Formula One Predictor',
+    desc: 'This project is aimed towards predicting the top half F1 GP winners based on information before the race',
+    github: 'https://github.com/R-otato/Formula-one-prediction',
   },
   {
-    name: 'Asteroid Destroyer (School Group Project)',
-    desc: 'An augmented reality game made using Unity3D',
-    link: 'https://drive.google.com/file/d/150NG31p5Ue8472zM-3ih8G53t47hV2zp/view?usp=sharing',
-    text: 'Video',
+    name: 'Singapore HDB Resale Price Predictor',
+    desc: 'This project aims to create a machine-learning model for predicting the resale prices of HDB flats using their properties from the data given. Using the model, we can find how the selling price of an HDB resale flat changes based on the information given.',
+    github: 'https://github.com/R-otato/PySpark-HDB-resale-prediction',
   },
 ];
 
-export const feedbacks = [
-  //   {
-  //     name: 'John Smith',
-  //     feedback:
-  //       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
-  //   },
-  //   {
-  //     name: 'John Smith',
-  //     feedback:
-  //       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
-  //   },
-];
+// export const feedbacks = [
+//   {
+//     name: 'John Smith',
+//     feedback:
+//       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+//   },
+//   {
+//     name: 'John Smith',
+//     feedback:
+//       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+//   },
+// ];
+
+// See object prototype on SEO.jsx page
+export const seoData = {
+  title: 'Ryan Liam',
+  description: 'A passionate Data Science Student.',
+};
