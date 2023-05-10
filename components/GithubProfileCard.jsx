738e3e32babb04c349d6d9f6d294f57730917a60
Ryan Liam <ryanliampy@gmail.com>
2023-05-10 11:05:39 +0800

    Update GithubProfileCard.jsx

--- components/GithubProfileCard.jsx
+++ components/GithubProfileCard.jsx
@@ -25,10 +25,10 @@ const GithubProfileCard = ({ prof }) => {
                 DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR
                 ALL
               </p>
-              <p className="text-white mt-3">{prof.bio}</p>
+              <p class="text-white mt-3">Aspiring Data Scientist</p>
               <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
-                <i className="ni ni-pin-3 text-info mr-2" />
-                {prof.location}
+                <i class="ni ni-pin-3 text-info mr-2"></i>
+                "Singapore"
               </div>
               <SocialLinks />
             </Col>
