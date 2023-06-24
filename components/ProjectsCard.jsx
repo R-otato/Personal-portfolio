52f37f5dc8d1b2563a8f22a55f4f67e5591dc432
Ryan Liam <ryanliampy@gmail.com>
2023-06-25 00:43:27 +0800

    dasd

--- components/ProjectsCard.jsx
+++ components/ProjectsCard.jsx
@@ -13,7 +13,10 @@ const ProjectsCard = ({ data }) => {
             <div className="d-flex px-3">
               <div className="pl-4">
                 <h3>{data.name}</h3>
-                <p className="description mt-3">{data.desc}</p>
+                <p
+                  className="description mt-3"
+                  dangerouslySetInnerHTML={{ __html: data.desc }}
+                />
                 {data.github ? (
                   <Button
                     className="btn-icon"
